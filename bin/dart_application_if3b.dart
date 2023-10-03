427ffffba19f9bdcacb66c64aa900ddbef36e5cb
VickyYanto <vickyyanto_2226250010@mhs.mdp.ac.id>
2023-10-03 12:57:22 +0700

    Tugas 1.2

--- bin/dart_application_if3b.dart
+++ bin/dart_application_if3b.dart
@@ -1,6 +1,28 @@
-import 'package:dart_application_if3b/dart_application_if3b.dart'
-    as dart_application_if3b;
+class PersegiPanjang {
+  int panjang, lebar;
 
-void main(List<String> arguments) {
-  print('Hello Everyone');
+  //Constructor dari class PersegiPanjang
+  PersegiPanjang(this.panjang, this.lebar);
+  //method hitung luas
+  int hitungLuas() {
+    return panjang * lebar;
+  }
+
+  //method hitung keliling
+  int hitungKeliling() {
+    return 2 * (panjang + lebar);
+  }
+}
+
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegiPanjang = PersegiPanjang(panjang, lebar);
+
+  int luasPersegi = persegiPanjang.hitungLuas();
+  int kelilingPersegi = persegiPanjang.hitungKeliling();
+
+  print("Luas Persegi Panjang : $luasPersegi");
+  print("Keliling Persegi Panjang : $kelilingPersegi");
 }
